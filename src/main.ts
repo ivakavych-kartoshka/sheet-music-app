8db1175bbb2b4966ffc62c0575c7e17499186ee7
PhamKhaVy <phamkhavycm2004@gmail.com>
2026-04-21 00:30:37 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+    origin: [
+      'http://localhost:3000',
+      'https://sheet-music-app-client.vercel.app',
+    ],
     credentials: true,
   });
 
